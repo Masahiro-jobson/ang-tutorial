fd356dedb981ef167340737abfa594e08ecfb6e2
masahiro <m.togasaki1026@gmail.com>
2025-12-16 08:51:44 +1100

    Second commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// @ - is decorator which decorates components and every component needs
+// decorator
 @Component({
+  // go into the webpage and wherever it finds app-root, it injects code in it.
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ang-tutorial');
+  title= "Project CC";
+  date = new Date();
 }
